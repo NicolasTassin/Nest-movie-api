633ca384bd16b9000bf5a9442ca4c876c653e260
NicolasTassin <54062897+NicolasTassin@users.noreply.github.com>
2023-10-07 12:30:15 +0200

    Created controllers and services for movies and genres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesController } from './movies/movies.controller';
+import { GenresController } from './genres/genres.controller';
+import { MoviesService } from './movies/movies.service';
+import { GenresService } from './genres/genres.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MoviesController, GenresController],
+  providers: [AppService, MoviesService, GenresService],
 })
 export class AppModule {}
---
+++ src/dto/movie.dto.ts
@@ -0,0 +1,6 @@
+export class CreateMovieDto {
+  title: string;
+  description: string;
+  releaseDate: Date;
+  genre: string[];
+}
